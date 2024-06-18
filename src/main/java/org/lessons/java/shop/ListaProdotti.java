f56156c0dae1293ad9d9e27dd44e1de9221a715e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-18 15:12:18 +0200

    add: aggiungiProdotti method added to ListaProdotti

--- src/main/java/org/lessons/java/shop/ListaProdotti.java
+++ src/main/java/org/lessons/java/shop/ListaProdotti.java
@@ -15,5 +15,28 @@ public class ListaProdotti {
 	
 	
 	
+	//Metodo per aggiungere uno o più prodotti all'array
 	
+		public void aggiungiProdotti(Prodotto...prodotti ) {
+			
+			//Creiamo un nuovo array di lunghezza pari a prodList + il parametro prodotti
+			Prodotto nuovaLista[] = new Prodotto[prodList.length + prodotti.length];
+			
+			//Copio gli elementi di prodList in nuovalista
+			for(int i=0; i<prodList.length; i++) {
+				
+				nuovaLista[i] = prodList[i];
+			}
+			
+			//Copio gli elementi di prodotti nelle restanti posizioni dell'array
+			
+			for(int i=0; i<prodotti.length; i++) {
+				
+				nuovaLista[i+prodList.length] = prodotti[i];
+			}
+			
+			//Sovrascrivo il riferimento di prodList con quello della nuova lista
+			
+			this.prodList = nuovaLista;
+		}
 }
