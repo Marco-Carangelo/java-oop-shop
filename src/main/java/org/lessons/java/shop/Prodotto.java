867c7998a4aabe6d34537d04c5c095a0b210ec5c
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-18 12:15:21 +0200

    add: Prodotto class
    Completamente dimenticato di fare i commits man mano questa volta

--- src/main/java/org/lessons/java/shop/Prodotto.java
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,139 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	
+	//Dichiarazione degli attributi della classe
+	
+	private int prodCode;
+	private String prodName;
+	private String prodInfo;
+	private float prodPrice;
+	private int prodIva;
+	
+	
+	//Metodo costruttore della classe
+	
+	public Prodotto(String prodName,String prodInfo,float prodPrice,int prodIva) {
+			
+		//Dichiarazione di una variabile locale per contenere un'istanza di Random
+		Random r = new Random();
+			
+		//Assegnazione di un valore random compreso tra le costanti MIN = 10000000 e Max = 99999999
+		
+		final int MIN = 0;
+		final int MAX = 99999999;
+		
+		this.prodCode= (int) (r.nextDouble() * (MAX - MIN + 1)) + MIN;
+			
+		//Assegnazione degli altri attributi del metodo
+		this.prodName =prodName;
+		this.prodInfo=prodInfo;
+		this.prodPrice=prodPrice;
+		this.prodIva=prodIva;
+	}
+	
+	
+	
+		
+	//Metodo per avare il prezzo base
+		
+	public String getProdName() {
+		return prodName;
+	}
+
+
+	//***METODI GETTER E SETTER DEGLI ATTRIBUTI DEGLI OGGETTI
+	
+	//L'attributo per il codice del prodotto sarà accessibile in sola lettura, qui avra solo il metodo setter
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+
+
+
+
+	public String getProdInfo() {
+		return prodInfo;
+	}
+
+
+
+
+	public void setProdInfo(String prodInfo) {
+		this.prodInfo = prodInfo;
+	}
+
+
+
+
+	public float getProdPrice() {
+		return prodPrice;
+	}
+
+
+
+
+	public void setProdPrice(float prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+
+
+
+
+	public int getProdIva() {
+		return prodIva;
+	}
+
+
+
+
+	public void setProdIva(int prodIva) {
+		this.prodIva = prodIva;
+	}
+
+
+
+
+	public int getProdCode() {
+		return prodCode;
+	}
+	
+	
+	
+		
+	//Metodo per avere il prezzo + IVA
+		
+	public float getTaxedPrice( ) {
+		
+		//Restituiamo il risultato della somma tra il prezzo del prodotto e la percentuale d'IVA
+		
+		return prodPrice + (prodPrice*prodIva/100);
+	}
+	
+	
+	
+		
+	//Metodo per ottenere il nome esteso formato da "codicenumerico-nome"
+		
+	public String getFullProdName(String code) {
+			
+		//Dichiariamo una variabile locale che conterrà il nome completo del prodotto
+		String fullName = String.format("%s-%s",code,prodName);
+		
+		return fullName;
+	}
+	
+	
+	
+	
+	//Metodo che prende come argomento un codice numerico e, se ha meno di 8 cifre, aggiunge 0 a sinistra per il numero di cifre mancanti 
+	static public String paddingCode(int code) {
+		
+		String paddedCode = String.format("%08d",code);
+		
+		return paddedCode;
+	}
+	
 }
