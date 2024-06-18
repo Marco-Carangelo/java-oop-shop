df8229cdc0d34488b817b066fbd466285d3859fb
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-06-18 15:20:21 +0200

    add: stampaProdotti method to print elements in prodList array

--- src/main/java/org/lessons/java/shop/ListaProdotti.java
+++ src/main/java/org/lessons/java/shop/ListaProdotti.java
@@ -15,28 +15,51 @@ public class ListaProdotti {
 	
 	
 	
+	
 	//Metodo per aggiungere uno o più prodotti all'array
 	
-		public void aggiungiProdotti(Prodotto...prodotti ) {
+	public void aggiungiProdotti(Prodotto...prodotti ) {
 			
-			//Creiamo un nuovo array di lunghezza pari a prodList + il parametro prodotti
-			Prodotto nuovaLista[] = new Prodotto[prodList.length + prodotti.length];
+		//Creiamo un nuovo array di lunghezza pari a prodList + il parametro prodotti
+		Prodotto nuovaLista[] = new Prodotto[prodList.length + prodotti.length];
 			
-			//Copio gli elementi di prodList in nuovalista
-			for(int i=0; i<prodList.length; i++) {
+		//Copio gli elementi di prodList in nuovalista
+		for(int i=0; i<prodList.length; i++) {
 				
-				nuovaLista[i] = prodList[i];
-			}
-			
-			//Copio gli elementi di prodotti nelle restanti posizioni dell'array
-			
-			for(int i=0; i<prodotti.length; i++) {
-				
-				nuovaLista[i+prodList.length] = prodotti[i];
-			}
-			
-			//Sovrascrivo il riferimento di prodList con quello della nuova lista
-			
-			this.prodList = nuovaLista;
+			nuovaLista[i] = prodList[i];
 		}
+			
+		//Copio gli elementi di prodotti nelle restanti posizioni dell'array
+			
+		for(int i=0; i<prodotti.length; i++) {
+				
+			nuovaLista[i+prodList.length] = prodotti[i];
+		}
+			
+		//Sovrascrivo il riferimento di prodList con quello della nuova lista
+			
+		this.prodList = nuovaLista;
+	}
+	
+	
+	
+	//Metodo per stampare tutti i prodotti nella lista
+	
+	public void stampaProdotti() {
+		
+		for(Prodotto prod:prodList) {
+			
+			System.out.println("********************************************");
+			System.out.println("Nome: " + prod.getProdName());
+			System.out.println("Descrizione: " + prod.getProdInfo());
+			System.out.println("Codice: " + prod.getFullProdName());
+			System.out.println("Prezzo di base: " + prod.getProdPrice() + " €");
+			System.out.println("Prezzo + IVA: " + prod.getTaxedPrice() + " €");
+			System.out.println("********************************************");
+			
+		}
+	}
+		
+		
+		
 }
